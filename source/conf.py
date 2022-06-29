7410881f3debd20624ef3f68b289f48143b772e1
priestmuhindo <priestmuhindo@gmail.com>
2022-06-29 10:02:27 +0200

    ajout du logo

--- source/conf.py
+++ source/conf.py
@@ -29,7 +29,9 @@ author = 'Priestley Muhindo'
 # ones.
 extensions = [
 ]
+# Add download button
 
+formats= 'pdf'
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -50,3 +52,11 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_logo = "maxqda.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
+
+
